4db1db60e8ea0df49d344333252e172417ac2785
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-02-12 13:36:25 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
 
 const openMenuHamburger = () => {
-    
+
     let menuHamburger = document.getElementById('menuHamburger')
 
     if (menuHamburger.style.display = 'none') {
         menuHamburger.style.display = 'flex'
+    }else{
+        alert('erro')
     }
+  
 
 };
+
+const teste = () => {
+    let menuHamburger = document.getElementById('menuHamburger')
+
+    if (menuHamburger.style.display = 'flex') {
+        menuHamburger.style.display = 'none'
+    }else{
+        alert('erro')
+    }}
 
 
 
