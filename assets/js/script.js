6f660dd39ea4758488246de32d1b4dad7051a361
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-02-12 13:03:54 -0300

    feat: menu clicked added

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+const openMenuHamburger = () => {
+
+    var menuHamburger = document.getElementById('menuHamburger')
+
+    if (menuHamburger.style.display = 'none') {
+        menuHamburger.style.display = 'flex'
+    }
+  
+};
+
+
